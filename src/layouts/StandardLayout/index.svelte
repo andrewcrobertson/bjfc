ed66344cc520fcec93d7b9862c84441eaa2bb6a6
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-13 22:11:22 +1100

    feat: wip

--- src/layouts/StandardLayout/index.svelte
+++ src/layouts/StandardLayout/index.svelte
@@ -5,6 +5,28 @@
   let open = false;
 </script>
 
+<style>
+  /* Hide scrollbar: https://stackoverflow.com/a/49278385/40167 */
+  div {
+    overflow: auto;
+    max-height: 90vh;
+    overflow-y: scroll;
+    /* Firefox */
+    scrollbar-width: none;
+    /* Internet Explorer 10+ */
+    -ms-overflow-style: none;
+  }
+
+  /* WebKit */
+  div::-webkit-scrollbar {
+    /* WebKit */
+    width: 0;
+    height: 0;
+  }
+</style>
+
 <Sidebar bind:open />
 <Navbar bind:sidebar={open} />
-<slot />
+<div>
+  <slot />
+</div>
