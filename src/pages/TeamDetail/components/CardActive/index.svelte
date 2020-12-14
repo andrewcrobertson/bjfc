b27247756bb168b68e70ee678f2b22ea03e3a561
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-15 07:20:12 +1100

    feat: wip

--- src/pages/TeamDetail/components/CardActive/index.svelte
+++ src/pages/TeamDetail/components/CardActive/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Responsive from '@this/components/Responsive';
   import { background } from '@this/constants/theme';
   import BadgeCheckIcon from '@this/icons/BadgeCheckIcon';
   import CurrencyDollarIcon from '@this/icons/CurrencyDollarIcon';
@@ -15,20 +16,39 @@
   const colour = background[gender] ?? 'gray';
 </script>
 
-<a class="{rootClass} p-2" href={url}>
-  <div class="flex justify-between">
-    <div class="flex items-center">
-      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
-      <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+<Responsive>
+  <a slot="sm" class="{rootClass} p-2" href={url}>
+    <div class="flex flex-col">
+      <div class="flex items-center">
+        <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
+        <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+      </div>
+      <span class="flex justify-end items-center leading-none text-sm mr-2 py-1">
+        {#if paidThisSeason}
+          <CurrencyDollarIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+          Paid
+        {:else}
+          <BadgeCheckIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+          Registered
+        {/if}
+      </span>
     </div>
-    <span class="mr-3 inline-flex items-center leading-none text-sm py-1">
-      {#if paidThisSeason}
-        <CurrencyDollarIcon class="w-5 h-5 mr-1 text-{colour}-500" />
-        Paid
-      {:else}
-        <BadgeCheckIcon class="w-5 h-5 mr-1 text-{colour}-500" />
-        Registered
-      {/if}
-    </span>
-  </div>
-</a>
+  </a>
+  <a slot="else" class="{rootClass} p-2" href={url}>
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
+        <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+      </div>
+      <span class="mr-3 inline-flex items-center leading-none text-sm py-1">
+        {#if paidThisSeason}
+          <CurrencyDollarIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+          Paid
+        {:else}
+          <BadgeCheckIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+          Registered
+        {/if}
+      </span>
+    </div>
+  </a>
+</Responsive>
--- src/pages/TeamDetail/components/CardTransfer/index.svelte
+++ src/pages/TeamDetail/components/CardTransfer/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Responsive from '@this/components/Responsive';
   import { background } from '@this/constants/theme';
   import LogoutIcon from '@this/icons/LogoutIcon';
 
@@ -14,15 +15,29 @@
   const colour = background[gender] ?? 'gray';
 </script>
 
-<a class="{rootClass} p-2" href={url}>
-  <div class="flex justify-between">
-    <div class="flex items-center">
-      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
-      <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+<Responsive>
+  <a slot="sm" class="{rootClass} p-2" href={url}>
+    <div class="flex flex-col">
+      <div class="flex items-center">
+        <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
+        <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+      </div>
+      <span class="flex justify-end items-center leading-none text-sm mr-2 py-1">
+        <LogoutIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+        {club}
+      </span>
     </div>
-    <span class="mr-3 inline-flex items-center leading-none text-sm py-1">
-      <LogoutIcon class="w-5 h-5 mr-1 text-{colour}-500" />
-      {club}
-    </span>
-  </div>
-</a>
+  </a>
+  <a slot="else" class="{rootClass} p-2" href={url}>
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
+        <h2 class="text-lg font-medium title-font ml-2">{familyName}, {firstName}</h2>
+      </div>
+      <span class="mr-3 inline-flex items-center leading-none text-sm py-1">
+        <LogoutIcon class="w-5 h-5 mr-1 text-{colour}-500" />
+        {club}
+      </span>
+    </div>
+  </a>
+</Responsive>
