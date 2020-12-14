07ff1bebb4a5f75d8a8a691e87a6d2ce91979ec5
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-14 15:54:02 +1100

    feat: wip

--- src/tools/etl/transform2/teamDetail.ts
+++ src/tools/etl/transform2/teamDetail.ts
@@ -1,11 +1,7 @@
 import each from 'lodash/each';
 import filter from 'lodash/filter';
-import groupBy from 'lodash/groupBy';
-import includes from 'lodash/includes';
-import last from 'lodash/last';
 import map from 'lodash/map';
 import orderBy from 'lodash/orderBy';
-import split from 'lodash/split';
 import type { ISanitisedConfig } from '../sanitisedConfig';
 import type { ISanitisedMember } from '../sanitisedMember';
 import type { ISanitisedTeam } from '../sanitisedTeam';
@@ -16,26 +12,20 @@ export interface Options {
   teams: ISanitisedTeam[];
 }
 
-export const teamDetail = ({ config, teams, members }: Options) => {
-  const membersByBirthYear = groupBy(members, (member) => parseInt(last(split(member.dateOfBirth, '/'))));
+export const teamDetail = ({ teams, members }: Options) => {
   const output: any = {};
 
-  each(teams, ({ code, name, birthYears, playerGenders }) => {
+  each(teams, ({ code, name }) => {
     const team = { name, members: [] };
-    each(birthYears, (birthYear) => {
-      const membersAll = membersByBirthYear[birthYear];
-      const membersFiltered = filter(membersAll, ({ gender }) => includes(playerGenders, gender));
-      const members = map(membersFiltered, ({ footyWebNumber, familyName, firstName, dateOfBirth, gender }) => ({
-        footyWebNumber,
-        familyName,
-        firstName,
-        dateOfBirth,
-        gender,
-      }));
-      team.members.push(...members);
-    });
-
-    team.members = orderBy(team.members, ['familyName', 'firstName'], ['asc', 'asc']);
+    const membersFiltered = filter(members, ({ teamCode }) => teamCode === code);
+    const membersMapped = map(membersFiltered, ({ footyWebNumber, familyName, firstName, dateOfBirth, gender }) => ({
+      footyWebNumber,
+      familyName,
+      firstName,
+      dateOfBirth,
+      gender,
+    }));
+    team.members = orderBy(membersMapped, ['familyName', 'firstName'], ['asc', 'asc']);
     output[code] = team;
   });
 
