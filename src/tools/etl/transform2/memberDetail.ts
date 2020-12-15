81fafc88cbec0d03d41a5389926f47738992577b
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-15 16:36:42 +1100

    feat: wip

--- src/tools/etl/transform2/memberDetail.ts
+++ src/tools/etl/transform2/memberDetail.ts
@@ -1,6 +1,8 @@
+import fromPairs from 'lodash/fromPairs';
 import keyBy from 'lodash/keyBy';
 import map from 'lodash/map';
 import orderBy from 'lodash/orderBy';
+import pick from 'lodash/pick';
 import type { ISanitisedConfig } from '../sanitisedConfig';
 import type { ISanitisedMember } from '../sanitisedMember';
 import type { ISanitisedTeam } from '../sanitisedTeam';
@@ -11,12 +13,28 @@ export interface Options {
   teams: ISanitisedTeam[];
 }
 
-export const memberDetail = ({ config, members: membersRaw }: Options) => {
-  const members = map(membersRaw, (member) => {
-    const transactions = orderBy(member.transactions, ['transactionDate', 'transactionTime'], ['desc', 'desc']);
-    const transfers = orderBy(member.transfers, ['applicationDate'], ['desc']);
-
-    return { ...member, transactions, transfers };
+const fields = [
+  'footyWebNumber',
+  'initials',
+  'lastName',
+  'firstName',
+  'dateOfBirth',
+  'club',
+  'gender',
+  'guardians',
+  'emergencyContact',
+  'contact',
+  'transactions',
+  'transfers',
+];
+export const memberDetail = ({ teams, members: membersRaw }: Options) => {
+  const teamMap = fromPairs(map(teams, ({ code, name }) => [code, name]));
+  const members = map(membersRaw, (memberRaw) => {
+    const member = pick(memberRaw, ...fields) as any;
+    member.transactions = orderBy(memberRaw.transactions, ['transactionDate', 'transactionTime'], ['desc', 'desc']);
+    member.transfers = orderBy(memberRaw.transfers, ['applicationDate'], ['desc']);
+    member.team = teamMap[memberRaw.teamCode] ?? null;
+    return member;
   });
 
   return keyBy(members, 'footyWebNumber');
