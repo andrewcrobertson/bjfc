58d3ebbca1926d5b8d4eb7a3e29adc31a4ac1e85
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-15 14:24:49 +1100

    feat: wip

--- src/components/PhoneButton/index.svelte
+++ src/components/PhoneButton/index.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-  import replace from 'lodash/replace';
+  import split from 'lodash/split';
+  import join from 'lodash/join';
   import PhoneIcon from '@this/icons/PhoneIcon';
   let rootClass = '';
   export { rootClass as class };
   export let phone = null;
   export let label = 'Phone';
   export let colour = 'gray';
+
+  const sanitisePhone = (phone: string) => join(split(phone, ' '));
 </script>
 
 {#if phone === null}
@@ -14,7 +17,7 @@
     {label}: Not Listed
   </div>
 {:else}
-  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{replace(phone, ' ', '')}">
+  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{sanitisePhone(phone)}">
     <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
     {label}:
     {phone}
