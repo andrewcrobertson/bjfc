37bf61fe07d2b190f4e28952a03fabcef9233055
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-16 09:16:05 +1100

    feat: wip

--- src/components/PhoneButton/index.svelte
+++ src/components/PhoneButton/index.svelte
@@ -17,7 +17,7 @@
     {label}: Not Listed
   </div>
 {:else}
-  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="sms:{sanitisePhone(phone)}">
+  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{sanitisePhone(phone)}">
     <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
     {label}:
     {phone}
