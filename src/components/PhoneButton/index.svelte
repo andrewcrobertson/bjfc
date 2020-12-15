4ddd8122a4bbfdee09a9d79e89d463aa55d1f1cb
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-15 14:11:28 +1100

    feat: wip

--- src/components/PhoneButton/index.svelte
+++ src/components/PhoneButton/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import replace from 'lodash/replace';
   import PhoneIcon from '@this/icons/PhoneIcon';
   let rootClass = '';
   export { rootClass as class };
@@ -13,7 +14,7 @@
     {label}: Not Listed
   </div>
 {:else}
-  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{phone}">
+  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{replace(phone, ' ', '')}">
     <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
     {label}:
     {phone}
--- src/components/Sidebar/index.svelte
+++ src/components/Sidebar/index.svelte
@@ -13,9 +13,9 @@
   }
 </style>
 
-<aside class="absolute w-4/5 h-full bg-gray-200 border-r-2 shadow-lg" class:open>
+<aside class="absolute w-11/12 md:w-96 h-full bg-gray-200 border-r-2 shadow-lg" class:open>
   <nav class="p-12 text-xl">
-    <a class="block" href="/" on:click={() => (open = false)}>Team List</a>
-    <a class="block" href="/committee/" on:click={() => (open = false)}>Committee</a>
+    <a class="block items-center border border-gray-300 px-6 py-3 " href="/" on:click={() => (open = false)}>Team List</a>
+    <a class="block items-center border border-gray-300 px-6 py-3 mt-2" href="/committee/" on:click={() => (open = false)}>Committee</a>
   </nav>
 </aside>
