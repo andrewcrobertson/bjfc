85563d03f0ffd72e372c67fb603c2c475b8b1ba0
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-16 09:10:05 +1100

    feat: wip

--- src/pages/MemberDetail/components/CardTransfer/index.svelte
+++ src/pages/MemberDetail/components/CardTransfer/index.svelte
@@ -4,14 +4,13 @@
   let rootClass = '';
   export { rootClass as class };
   export let applicationDate = null;
-  export let sourceClub = null;
   export let destinationClub = '';
   export let colour = 'gray';
 </script>
 
 <div class="{rootClass} p-2">
   <div class="flex flex-col sm:flex-row sm:justify-between">
-    <h2 class="text-lg font-medium title-font ml-2">{sourceClub} &#8594; {destinationClub}</h2>
+    <h2 class="text-lg font-medium title-font ml-2">{destinationClub}</h2>
     <div class="inline-flex items-center text-sm ml-2 py-1">
       <LogoutIcon class="text-{colour}-500 w-5 h-5 mr-1" />
       {applicationDate}
--- src/pages/MemberDetail/components/PanelTransfer/index.svelte
+++ src/pages/MemberDetail/components/PanelTransfer/index.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-wrap sm:p-4 mt-2">
   {#each records as { applicationDate, sourceClub, destinationClub }, i}
-    <CardTransfer class="w-full border-l border-r border-b border-gray-300 {i === 0 ? 'border-t' : ''}" {applicationDate} {sourceClub} {destinationClub} />
+    <CardTransfer class="w-full border-l border-r border-b border-gray-300 {i === 0 ? 'border-t' : ''}" {applicationDate} {destinationClub} />
   {:else}
     <p class="p-4">There are no transfer records</p>
   {/each}
