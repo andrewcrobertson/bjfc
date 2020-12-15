c6f72c37fca4db768f735bde186d0256e049d352
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-15 11:54:57 +1100

    feat: wip

--- src/tools/etl/transform2/teamOfficialList.ts
+++ src/tools/etl/transform2/teamOfficialList.ts
@@ -1,8 +1,4 @@
-import { pick } from 'lodash';
-import each from 'lodash/each';
-import filter from 'lodash/filter';
-import map from 'lodash/map';
-import orderBy from 'lodash/orderBy';
+import { fromPairs, map } from 'lodash';
 import type { ISanitisedConfig } from '../sanitisedConfig';
 import type { ISanitisedMember } from '../sanitisedMember';
 import type { ISanitisedTeam } from '../sanitisedTeam';
@@ -13,45 +9,5 @@ export interface Options {
   teams: ISanitisedTeam[];
 }
 
-export const teamOfficialList = ({ teams, members }: Options) => {
-  const output: any = {};
-
-  each(teams, ({ code, name, headCoach, assistantCoach, teamManager, trainer }) => {
-    const team: any = { name, membersRegisteredThisSeason: [], membersRegisteredRecently: [], membersTransferred: [], membersOther: [] };
-    const teamMembersFiltered = filter(members, ({ teamCode }) => teamCode === code);
-
-    each(teamMembersFiltered, (member) => {
-      if (member.club !== 'Bayswater') {
-        team.membersTransferred.push(member);
-      } else if (member.registeredThisSeason) {
-        team.membersRegisteredThisSeason.push(member);
-      } else if (member.registeredRecently) {
-        team.membersRegisteredRecently.push(member);
-      } else {
-        team.membersOther.push(member);
-      }
-    });
-
-    team.membersRegisteredThisSeason = map(team.membersRegisteredThisSeason, (obj) =>
-      pick(obj, 'footyWebNumber', 'initials', 'familyName', 'firstName', 'gender', 'paidThisSeason')
-    );
-    team.membersRegisteredThisSeason = orderBy(team.membersRegisteredThisSeason, ['familyName', 'firstName'], ['asc', 'asc']);
-
-    team.membersRegisteredRecently = map(team.membersRegisteredRecently, (obj) => pick(obj, 'footyWebNumber', 'initials', 'familyName', 'firstName', 'gender'));
-    team.membersRegisteredRecently = orderBy(team.membersRegisteredRecently, ['familyName', 'firstName'], ['asc', 'asc']);
-
-    team.membersTransferred = map(team.membersTransferred, (obj) => pick(obj, 'footyWebNumber', 'initials', 'familyName', 'firstName', 'gender', 'club'));
-    team.membersTransferred = orderBy(team.membersTransferred, ['familyName', 'firstName'], ['asc', 'asc']);
-
-    team.membersOther = map(team.membersOther, (obj) => pick(obj, 'footyWebNumber', 'initials', 'familyName', 'firstName', 'gender'));
-    team.membersOther = orderBy(team.membersOther, ['familyName', 'firstName'], ['asc', 'asc']);
-
-    team.headCoach = headCoach;
-    team.assistantCoach = assistantCoach;
-    team.teamManager = teamManager;
-    team.trainer = trainer;
-    output[code] = team;
-  });
-
-  return output;
-};
+export const teamOfficialList = ({ teams, members }: Options) =>
+  fromPairs(map(teams, ({ code, name, headCoach, assistantCoach, teamManager, trainer }) => [code, { name, headCoach, assistantCoach, teamManager, trainer }]));
