e93f68d6e43a03efebb10d342019b255e4bfc074
Lara Robertson <laraejrobertson@gmail.com>
2020-12-17 09:27:38 +1100

    feat: truncate text in email button

--- src/components/EmailButton/index.svelte
+++ src/components/EmailButton/index.svelte
@@ -14,6 +14,6 @@
 {:else}
   <a class="{rootClass} inline-flex items-center text-sm border border-gray-300 px-3 py-3" href="mailto:{email}">
     <AtSymbolIcon class="w-5 h-5 mr-2 text-{colour}-500" />
-    {email}
+    <span class="truncate">{email}</span>
   </a>
 {/if}
