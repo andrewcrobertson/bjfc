fe97163231f342f2c1f7096d5ad44832c9a236a8
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-17 10:47:23 +1100

    feat: wip

--- src/components/AvatarHeading/index.svelte
+++ src/components/AvatarHeading/index.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
+  import map from 'lodash/map';
+  import join from 'lodash/join';
+  import split from 'lodash/split';
+
   let rootClass = '';
   export { rootClass as class };
-  export let initials = '';
+  export let initials = null;
   export let text = '';
   export let colour = 'gray';
+
+  const i =
+    initials ??
+    join(
+      map(split(text.replace(/[^a-z0-9 ]/gi, ''), ' '), (w) => w[0]),
+      ''
+    ).toUpperCase();
 </script>
 
 <div class="{rootClass} flex items-center">
-  <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{initials}</div>
+  <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-{colour}-100 text-{colour}-500">{i}</div>
   <h2 class="text-lg font-medium title-font ml-2">{text}</h2>
 </div>
--- src/pages/PlayerDetailMore/index.svelte
+++ src/pages/PlayerDetailMore/index.svelte
@@ -1,35 +1,41 @@
 <script lang="ts">
   import AvatarHeading from '@this/components/AvatarHeading';
-  import { Tabs, TabList, TabPanel, Tab } from '@this/components/Tabs';
   import { pageTitleStore as pageTitle } from '@this/layouts/StandardLayout';
-  import PanelTransaction from './components/PanelTransaction';
-  import PanelTransfer from './components/PanelTransfer';
+  import CardTransaction from './components/CardTransaction';
+  import CardTransfer from './components/CardTransfer';
   import players from './data';
   export let code: string = null;
 
   const player = players[code];
   const footyWebNumber = player.footyWebNumber;
   const colour = 'gray';
-  pageTitle.set('Player');
+  pageTitle.set('Player (More)');
 </script>
 
 <div class="max-w-6xl mx-auto xl:py-24">
   <div class="flex flex-col sm:flex-row sm:justify-between pb-5">
-    <AvatarHeading class="flex items-center space-x-2" {colour} initials={player.initials} text="{player.firstName} {player.lastName} (More)" />
+    <AvatarHeading class="flex items-center space-x-2" {colour} text="{player.firstName} {player.lastName}" />
     <div class="flex justify-end items-center text-sm mr-2 py-1">
       <a class="text-sm border border-gray-300 px-3 py-1" href="/players/{footyWebNumber}/">Main</a>
     </div>
   </div>
-  <Tabs class="max-w-6xl mx-auto xl:pb-24">
-    <TabList>
-      <Tab class="focus:outline-none inline-block rounded-t py-2 px-4" selectedClass="border-b border-gray-300">Transactions</Tab>
-      <Tab class="focus:outline-none inline-block rounded-t py-2 px-4" selectedClass="border-b border-gray-300">Transfers</Tab>
-    </TabList>
-    <TabPanel>
-      <PanelTransaction records={player.transactions} />
-    </TabPanel>
-    <TabPanel>
-      <PanelTransfer records={player.transfers} />
-    </TabPanel>
-  </Tabs>
+  <div class="flex flex-wrap sm:p-4 mt-2">
+    {#each player.transfers as { applicationDate, sourceClub, destinationClub }, i}
+      <CardTransfer class="w-full border-l border-r border-b border-gray-300 {i === 0 ? 'border-t' : ''}" {applicationDate} {destinationClub} />
+    {:else}
+      <p class="p-4">There are no transfer records</p>
+    {/each}
+  </div>
+  <div class="flex flex-wrap sm:p-4 mt-2">
+    {#each player.transactions as { transactionDate, product, lineItemTotal, transactionStatus }, i}
+      <CardTransaction
+        class="w-full border-l border-r border-b border-gray-300 {i === 0 ? 'border-t' : ''}"
+        {transactionDate}
+        {product}
+        {lineItemTotal}
+        {transactionStatus} />
+    {:else}
+      <p class="p-4">There are no transaction records</p>
+    {/each}
+  </div>
 </div>
