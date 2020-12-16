44da5ae4dcfb6c7a0d48a462a24d363f6661cb5d
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-16 13:35:16 +1100

    feat: git push

--- src/pages/TeamList/index.svelte
+++ src/pages/TeamList/index.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import { onDestroy, onMount } from 'svelte';
   import { pageTitleStore as pageTitle } from '@this/layouts/StandardLayout';
   import Card from './components/Card';
   import teams from './data';
 
   pageTitle.set('Team List');
+
+  onMount(async () => {
+    const result = await fetch('https://passport.sportstg.com/account/?', { mode: 'no-cors' });
+    console.log(result);
+  });
 </script>
 
 <div class="max-w-6xl mx-auto xl:py-24">
