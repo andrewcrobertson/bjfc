85ae92ad80da766972b032a2e981827e71c63b64
Lara Robertson <laraejrobertson@gmail.com>
2020-12-17 10:48:11 +1100

    feat: remove extra padding at top of page in laptop screen view

--- src/pages/TeamDashboard/index.svelte
+++ src/pages/TeamDashboard/index.svelte
@@ -6,7 +6,7 @@
   pageTitle.set('Team List');
 </script>
 
-<div class="container mx-auto xl:py-24">
+<div class="container mx-auto xl:py-10">
   <div class="flex flex-wrap -m-4 sm:p-4">
     {#each teams as team}
       <TeamCard class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4" {team} />
