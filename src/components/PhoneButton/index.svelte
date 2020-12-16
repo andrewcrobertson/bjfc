49a571631da79c3ccf831475a9e247cbabbdeb0f
Lara Robertson <laraejrobertson@gmail.com>
2020-12-16 17:35:24 +1100

    feat: changing phone button to phone and sms button group

--- src/components/PhoneButton/index.svelte
+++ src/components/PhoneButton/index.svelte
@@ -2,6 +2,7 @@
   import split from 'lodash/split';
   import join from 'lodash/join';
   import PhoneIcon from '@this/icons/PhoneIcon';
+  import ChatAltIcon from '@this/icons/ChatAltIcon';
   let rootClass = '';
   export { rootClass as class };
   export let phone = null;
@@ -11,15 +12,25 @@
   const sanitisePhone = (phone: string) => join(split(phone, ' '), '');
 </script>
 
-{#if phone === null}
-  <div class="{rootClass} cursor-default px-6 py-3 inline-flex items-center leading-none text-sm bg-gray-100 border border-gray-300">
-    <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
-    {label}: Not Listed
-  </div>
-{:else}
-  <a class="{rootClass} px-6 py-3 inline-flex items-center leading-none text-sm border border-gray-300" href="tel:{sanitisePhone(phone)}">
-    <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
-    {label}:
-    {phone}
-  </a>
-{/if}
+<div class="flex">
+  {#if phone === null}
+    <div class="{rootClass} cursor-default inline-flex flex-grow items-center leading-none text-sm bg-gray-100 border border-gray-300 px-6 py-3 ">
+      <PhoneIcon class="inline w-5 h-5 mr-1 text-{colour}-500" />
+      {label}: Not Listed
+    </div>
+    <a
+      class="{rootClass} inline-flex items-center leading-none text-sm bg-gray-100 border border-gray-300 border-l-0 px-6 py-3"
+      href="sms:{sanitisePhone(phone)}">
+      <ChatAltIcon class="inline w-5 h-5 text-{colour}-500 mr-1" />
+    </a>
+  {:else}
+    <a class="{rootClass} inline-flex flex-grow items-center leading-none text-sm border border-gray-300 px-6 py-3 " href="tel:{sanitisePhone(phone)}">
+      <PhoneIcon class="inline w-5 h-5 text-{colour}-500 mr-1" />
+      {label}:
+      {phone}
+    </a>
+    <a class="{rootClass} inline-flex items-center leading-none text-sm border border-gray-300 border-l-0 px-6 py-3 " href="sms:{sanitisePhone(phone)}">
+      <ChatAltIcon class="inline w-5 h-5 text-{colour}-500 mr-1" />
+    </a>
+  {/if}
+</div>
