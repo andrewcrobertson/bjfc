842869b53fdbc562b41189606049ab03c8e11a89
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-18 11:55:56 +1100

    feat: wip

--- src/tools/etl/transform1/teams/transformOfficial.ts
+++ src/tools/etl/transform1/teams/transformOfficial.ts
@@ -2,11 +2,5 @@ import type { ISanitisedOfficial } from '../../sanitisedTeam';
 import type { IRawTeamOfficial } from '../../types/rawTeam';
 import { toInitials } from '../utility/toInitials';
 
-export const transformOfficial = (official: IRawTeamOfficial): ISanitisedOfficial => {
-  if (official === null) {
-    return null;
-  }
-
-  const initials = toInitials(official.firstName, official.lastName);
-  return { ...official, initials };
-};
+export const transformOfficial = (official: IRawTeamOfficial): ISanitisedOfficial =>
+  official === null ? null : { ...official, initials: toInitials(official.firstName, official.lastName) };
