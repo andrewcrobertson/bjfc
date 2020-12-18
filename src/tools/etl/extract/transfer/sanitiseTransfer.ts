ef1e2c3249d55d7df9342ad143840195bebb6db2
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-18 13:30:31 +1100

    feat: wip

--- src/tools/etl/extract/transfer/sanitiseTransfer.ts
+++ src/tools/etl/extract/transfer/sanitiseTransfer.ts
@@ -3,11 +3,14 @@ import { sanitiseDashDateTime } from '../utility/sanitiseDashDateTime';
 import { sanitiseSlashDate } from '../utility/sanitiseSlashDate';
 import { sanitiseString } from '../utility/sanitiseString';
 
-export const sanitiseTransfer = (obj: any): IRawTransfer => ({
-  footyWebNumber: sanitiseString(obj.footyWebNumber),
-  transferYear: obj.transferYear,
-  applicationDate: sanitiseSlashDate(obj.applicationDate),
-  finalisedDate: sanitiseDashDateTime(obj.finalisedDate).date,
-  sourceClub: sanitiseString(obj.sourceClub),
-  destinationClub: sanitiseString(obj.destinationClub),
-});
+export const sanitiseTransfer = (obj: any): IRawTransfer =>
+  (obj.overallTransferStatus === 'Approved') === null
+    ? null
+    : {
+        footyWebNumber: sanitiseString(obj.footyWebNumber),
+        transferYear: obj.transferYear,
+        applicationDate: sanitiseSlashDate(obj.applicationDate),
+        finalisedDate: sanitiseDashDateTime(obj.finalisedDate).date,
+        sourceClub: sanitiseString(obj.sourceClub),
+        destinationClub: sanitiseString(obj.destinationClub),
+      };
