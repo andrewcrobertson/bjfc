326ee09b1a1362e648021a390d5cb9a302a15f13
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-18 11:54:26 +1100

    feat: wip

--- src/tools/etl/transform1/teams/transformTeams.ts
+++ src/tools/etl/transform1/teams/transformTeams.ts
@@ -5,21 +5,22 @@ import type { IRawTeam } from '../../types/rawTeam';
 import { transformGender } from './transformGender';
 import { transformOfficial } from './transformOfficial';
 
+const mapCommittee = (team: IRawTeam, seasonYear: number) => ({
+  code: team.code,
+  ageGroupCode: 'U' + Math.max(...team.ages).toString(),
+  name: team.name,
+  birthYears: map(team.ages, (age) => seasonYear - age),
+  playerGenders: team.genders,
+  teamGender: transformGender(team.genders),
+  headCoach: transformOfficial(team.headCoach),
+  assistantCoach: transformOfficial(team.assistantCoach),
+  trainer: transformOfficial(team.trainer),
+  teamManager: transformOfficial(team.teamManager),
+});
+
 export interface Options {
   config: IRawConfig;
   teams: IRawTeam[];
 }
 
-export const transformTeams = (options: Options): ISanitisedTeam[] =>
-  map(options.teams, (team) => ({
-    code: team.code,
-    ageGroupCode: 'U' + Math.max(...team.ages).toString(),
-    name: team.name,
-    birthYears: map(team.ages, (age) => options.config.seasonYear - age),
-    playerGenders: team.genders,
-    teamGender: transformGender(team.genders),
-    headCoach: transformOfficial(team.headCoach),
-    assistantCoach: transformOfficial(team.assistantCoach),
-    trainer: transformOfficial(team.trainer),
-    teamManager: transformOfficial(team.teamManager),
-  }));
+export const transformTeams = (options: Options): ISanitisedTeam[] => map(options.teams, (team) => mapCommittee(team, options.config.seasonYear));
