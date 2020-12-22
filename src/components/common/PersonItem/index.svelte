2aac8d49d28ef5ac4d0880d5117fbce33c18adec
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-22 14:32:27 +1100

    feat: wip

--- src/components/common/PersonItem/index.svelte
+++ src/components/common/PersonItem/index.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import * as personGenderEnum from '@this/constants/personGenderEnum';
   import { personBackground } from '@this/constants/theme';
-  import EmojiHappyIcon from '@this/components/icon/EmojiHappyIcon';
+  import PersonLabelledIcon from '@this/components/labelledIcon/PersonLabelledIcon';
+
   let rootClass = '';
   export { rootClass as class };
   export let text = '';
   export let colour = personBackground[personGenderEnum.unknown];
 </script>
 
-<div class="{rootClass} cursor-default inline-flex items-center text-sm border border-gray-300 border-dotted px-3 py-3">
-  <EmojiHappyIcon class="inline w-5 h-5 mr-2 text-{colour}-500" />{text}
+<div class="{rootClass} inline-flex items-center text-sm border border-gray-300 border-dotted px-3 py-3">
+  <PersonLabelledIcon iconClass="text-{colour}-500" {text} />
 </div>
---
+++ src/components/labelledIcon/PersonLabelledIcon/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import EmojiHappyIcon from '@this/components/icon/EmojiHappyIcon';
+  import LabelledIcon from '@this/components/labelledIcon/LabelledIcon';
+
+  let rootClass = '';
+  export { rootClass as class };
+  export let iconClass = '';
+  export let text: string = null;
+</script>
+
+<LabelledIcon class={rootClass} {iconClass} icon={EmojiHappyIcon}>{text}</LabelledIcon>
