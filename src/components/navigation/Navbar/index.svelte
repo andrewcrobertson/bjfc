4f12256be7ebaf757793b2deb77b3ff75ff6fac9
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-22 20:23:01 +1100

    feat: wip

--- src/components/navigation/Navbar/index.svelte
+++ src/components/navigation/Navbar/index.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import HomeIcon from '@this/components/icon/HomeIcon';
   import Hamburger from '../Hamburger';
 
   export let sidebar = false;
@@ -7,9 +6,8 @@
 </script>
 
 <header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
-  <nav class="flex space-x-2 items-center">
+  <nav class="flex items-center">
     <Hamburger bind:open={sidebar} />
-    <a class="flex items-center border border-gray-300 px-1 py-1" href="/"><HomeIcon class="w-7 h-7" /></a>
-    <p class="text-lg font-medium title-font">{label}</p>
+    <div class="text-lg font-medium title-font px-3 py-1">{label}</div>
   </nav>
 </header>
