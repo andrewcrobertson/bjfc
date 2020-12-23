08e750a06b52d03cda6ec799964b35cc33dca67c
andrewcrobertson <andrewcrobertson@users.noreply.github.com>
2020-12-23 11:35:17 +0000

    deploy: d989c31915e638b20b39559684a7279dd5a96c6f

---
+++ client/legacy/isString.0db50e72.js
@@ -0,0 +1,1 @@
+import{i as r,p as a,q as t}from"./map.7c409eae.js";var n=function(r){for(var a=-1,t=null==r?0:r.length,n=0,e=[];++a<t;){var o=r[a];o&&(e[n++]=o)}return e};var e=function(n){return"string"==typeof n||!r(n)&&a(n)&&"[object String]"==t(n)};export{n as c,e as i};
