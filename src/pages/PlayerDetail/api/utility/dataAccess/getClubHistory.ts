b47bc2eedfd927a6cb0948aee7b284d98b5bffab
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-28 20:13:10 +1100

    feat: wip

--- src/pages/PlayerDetail/api/utility/dataAccess/getClubHistory.ts
+++ src/pages/PlayerDetail/api/utility/dataAccess/getClubHistory.ts
@@ -3,7 +3,8 @@ import type { Database } from 'better-sqlite3';
 const sql = `SELECT club,
   date
 FROM playerClubHistory
-WHERE footyWebNumber = @footyWebNumber;`;
+WHERE footyWebNumber = @footyWebNumber
+ORDER BY date DESC;`;
 
 export interface IClubHistoryDb {
   club: string;
