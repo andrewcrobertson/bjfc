85f5846e8bcf0058b6e0103adebe80d0ae6f6828
Andrew Robertson <andrewcrobertson@gmail.com>
2021-02-03 12:42:20 +1100

    feat: wip

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+const Database = require('better-sqlite3');
 const { chromium } = require('playwright');
+const path = require('path');
+
+const sql = `SELECT code,
+  name,
+  topAge,
+  gender
+FROM team
+ORDER BY topAge,
+  gender;`;
+
+const headerTemplate = (name) => `<table style="width:100%; font-size: 8px;">
+  <tr>
+    <td style="text-align:left; padding-left: 10px">BJFC ${name}</td>
+    <td style="text-align:right; padding-right: 10px">Page <span class="pageNumber"></span> of <span class="totalPages"></span></td>
+  </tr>
+</table>`;
 
 (async () => {
-  const teams = ['U08M', 'U09M', 'U10M', 'U11M', 'U12B', 'U12G', 'U14B', 'U14G', 'U16G', 'U17B-GOLD', 'U17B-GREEN', 'U18G'];
+  const dbFile = path.resolve(__dirname, 'data', 'team.db');
+  const db = new Database(dbFile);
+  const teams = db.prepare(sql).all();
+
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
   for (let i = 0; i < teams.length; i++) {
-    await page.goto(`http://localhost:3000/teams/${teams[i]}/contact`);
-    await page.pdf({ path: `static/pdfs/${teams[i]}.pdf` });
+    const { code, name, topAge, gender } = teams[i];
+    await page.goto(`http://localhost:3000/teams/${code}/contact`);
+    await page.pdf({
+      path: `static/pdfs/${code}.pdf`,
+      format: 'A4',
+      headerTemplate: headerTemplate(name),
+      footerTemplate: '<span />',
+      displayHeaderFooter: true,
+      margin: { top: '70px', bottom: '70px' },
+    });
   }
 
   await browser.close();
--- src/pages/TeamContactList/index.svelte
+++ src/pages/TeamContactList/index.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-  import AvatarHeading from '@this/components/common/AvatarHeading';
   import { pageTitleStore as pageTitle } from '@this/layouts/StandardLayout';
   import PanelPlayer from './components/PanelPlayer';
   import type { IState } from './state';
   import logoUrl from './logo.jpg';
   export let state: IState;
-
-  const initials = state.ageGroupCode;
-  const text = state.name;
 
   pageTitle.set('Contact');
 </script>
@@ -26,11 +22,11 @@
   }
 </style>
 
-<div class="container py-10">
+<div class="container">
   <img id="watermark" src={logoUrl} alt="Warermark" />
-  <AvatarHeading colour="gray" {initials} {text} />
+  <h2 class="text-lg font-medium title-font pb-2">{state.name}</h2>
   <PanelPlayer class="pt-2 pb-2" players={state.playersActive} heading="Active Players" emptyMessage="There are no active players" />
   <PanelPlayer class="pt-2 pb-2" players={state.playersRecent} heading="Recent Players" emptyMessage="There are no recent players" />
   <PanelPlayer class="pt-2 pb-2" players={state.playersHistorical} heading="Historical Players" emptyMessage="There are no historical players" />
-  <PanelPlayer class="pt-2 pb-2" players={state.playersTransferred} heading="Transferred Players" emptyMessage="There are no transferred players" />
+  <!-- <PanelPlayer class="pt-2 pb-2" players={state.playersTransferred} heading="Transferred Players" emptyMessage="There are no transferred players" /> -->
 </div>
