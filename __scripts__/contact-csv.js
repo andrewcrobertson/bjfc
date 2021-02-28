e3bc2ed422fe280e800fdeb2648ec9dd9d5bbcff
Andrew Robertson <andrewcrobertson@gmail.com>
2021-02-28 21:04:11 +1100

    feat: wip

---
+++ __scripts__/contact-csv.js
@@ -0,0 +1,69 @@
+const Database = require('better-sqlite3');
+const fs = require('fs');
+const template = require('lodash/template');
+const path = require('path');
+const format = require('xml-formatter');
+
+const allTeamSql = `SELECT code, name FROM team ORDER BY topAge, gender;`;
+
+const teamPlayerActiveSql = `SELECT p.lastName || ', ' || p.firstName playerName,
+  COALESCE(pcg1.lastName || ', ' || pcg1.firstName, '') guardian1Name,
+  COALESCE(pcg1.email, '') guardian1Email,
+  COALESCE(pcg1.mobile, pcg1.phone1, pcg1.phone2, '') guardian1Phone,
+  COALESCE(pcg2.lastName || ', ' || pcg2.firstName, '') guardian2Name,
+  COALESCE(pcg1.email, '') guardian2Email,
+  COALESCE(pcg2.mobile, pcg2.phone1, pcg2.phone2, '') guardian2Phone
+FROM player p
+LEFT JOIN playerContactGuardian pcg1 ON p.footyWebNumber = pcg1.footyWebNumber AND pcg1.sequence = 1
+LEFT JOIN playerContactGuardian pcg2 ON p.footyWebNumber = pcg2.footyWebNumber AND pcg2.sequence = 2
+LEFT JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
+WHERE p.teamCode = @teamCode
+  AND p.status IN ('Registered', 'Insured')
+ORDER BY p.lastName, p.firstName;`;
+
+const teamPlayerRecentSql = `SELECT p.lastName || ', ' || p.firstName playerName,
+  COALESCE(pcg1.lastName || ', ' || pcg1.firstName, '') guardian1Name,
+  COALESCE(pcg1.email, '') guardian1Email,
+  COALESCE(pcg1.mobile, pcg1.phone1, pcg1.phone2, '') guardian1Phone,
+  COALESCE(pcg2.lastName || ', ' || pcg2.firstName, '') guardian2Name,
+  COALESCE(pcg1.email, '') guardian2Email,
+  COALESCE(pcg2.mobile, pcg2.phone1, pcg2.phone2, '') guardian2Phone
+FROM player p
+LEFT JOIN playerContactGuardian pcg1 ON p.footyWebNumber = pcg1.footyWebNumber AND pcg1.sequence = 1
+LEFT JOIN playerContactGuardian pcg2 ON p.footyWebNumber = pcg2.footyWebNumber AND pcg2.sequence = 2
+LEFT JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
+WHERE p.teamCode = @teamCode
+  AND p.status IN ('Recent')
+ORDER BY p.lastName, p.firstName;`;
+
+const teamPlayerHistoricalSql = `SELECT p.lastName || ', ' || p.firstName playerName,
+  COALESCE(pcg1.lastName || ', ' || pcg1.firstName, '') guardian1Name,
+  COALESCE(pcg1.email, '') guardian1Email,
+  COALESCE(pcg1.mobile, pcg1.phone1, pcg1.phone2, '') guardian1Phone,
+  COALESCE(pcg2.lastName || ', ' || pcg2.firstName, '') guardian2Name,
+  COALESCE(pcg1.email, '') guardian2Email,
+  COALESCE(pcg2.mobile, pcg2.phone1, pcg2.phone2, '') guardian2Phone
+FROM player p
+LEFT JOIN playerContactGuardian pcg1 ON p.footyWebNumber = pcg1.footyWebNumber AND pcg1.sequence = 1
+LEFT JOIN playerContactGuardian pcg2 ON p.footyWebNumber = pcg2.footyWebNumber AND pcg2.sequence = 2
+LEFT JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
+WHERE p.teamCode = @teamCode
+  AND p.status IN ('Historical')
+ORDER BY p.lastName, p.firstName;`;
+
+(async () => {
+  const dbFile = path.resolve(__dirname, '..', 'data', 'team.db');
+  const db = new Database(dbFile);
+  const teams = db.prepare(allTeamSql).all();
+  const compiled = template(fs.readFileSync(path.resolve(__dirname, 'template.xml')));
+
+  for (let i = 0; i < teams.length; i++) {
+    const { code, name } = teams[i];
+    const activePlayers = db.prepare(teamPlayerActiveSql).all({ teamCode: code });
+    const recentPlayers = db.prepare(teamPlayerRecentSql).all({ teamCode: code });
+    const historicalPlayers = db.prepare(teamPlayerHistoricalSql).all({ teamCode: code });
+    const text = format(compiled({ teamCode: code, activePlayers, recentPlayers, historicalPlayers }), { indentation: '  ', collapseContent: true });
+    const file = path.resolve(__dirname, '..', 'static', 'contact', `${code}.xml`);
+    fs.writeFileSync(file, text);
+  }
+})();
---
+++ __scripts__/contact-pdf.js
@@ -0,0 +1,42 @@
+const Database = require('better-sqlite3');
+const { chromium } = require('playwright');
+const path = require('path');
+
+const allTeamSql = `SELECT code,
+  name,
+  topAge,
+  gender
+FROM team
+ORDER BY topAge,
+  gender;`;
+
+const headerTemplate = (name) => `<table style="width:100%; font-size: 8px;">
+  <tr>
+    <td style="text-align:left; padding-left: 10px">BJFC ${name}</td>
+    <td style="text-align:right; padding-right: 10px">Page <span class="pageNumber"></span> of <span class="totalPages"></span></td>
+  </tr>
+</table>`;
+
+(async () => {
+  const dbFile = path.resolve(__dirname, '..', 'data', 'team.db');
+  const db = new Database(dbFile);
+  const teams = db.prepare(allTeamSql).all();
+
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  for (let i = 0; i < teams.length; i++) {
+    const { code, name, topAge, gender } = teams[i];
+    await page.goto(`http://localhost:3000/teams/${code}/contact`);
+    await page.pdf({
+      path: `static/contact/${code}.pdf`,
+      format: 'A4',
+      headerTemplate: headerTemplate(name),
+      footerTemplate: '<span />',
+      displayHeaderFooter: true,
+      margin: { top: '70px', bottom: '70px' },
+    });
+  }
+
+  await browser.close();
+})();
