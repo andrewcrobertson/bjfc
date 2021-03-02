6d00bcb8149a73255e1ab14fb581a74af9aa7996
Andrew Robertson <andrewcrobertson@gmail.com>
2021-03-03 08:53:36 +1100

    feat: wip

--- __scripts__/email.js
+++ __scripts__/email.js
@@ -22,7 +22,7 @@ const teamPlayerEmailActiveAndRecentSql = `SELECT DISTINCT a.email
   WHERE a.email IS NOT NULL
   ORDER BY a.email;`;
 
-const teamOfficialEmailSql = `SELECT tof.email FROM teamOfficial tof
+const teamOfficialEmailSql = `SELECT DISTINCT tof.email FROM teamOfficial tof
 WHERE tof.teamCode = @teamCode
   AND tof.email IS NOT NULL
 ORDER BY tof.email;`;
@@ -43,13 +43,13 @@ ORDER BY tof.email;`;
       '\n------------------------------' +
       '\n\nOfficials\n------------------------------\n' +
       teamOfficialEmail.map((r) => r.email).join(';\n') +
+      ';' +
       '\n\nActive Players\n------------------------------\n' +
       teamPlayerEmailActive.map((r) => r.email).join(';\n') +
+      ';' +
       '\n\nActive and Recent Players\n------------------------------\n' +
-      teamPlayerEmailActiveAndRecent
-        .map((r) => r.email)
-        .join(';\n')
-        .trim();
+      teamPlayerEmailActiveAndRecent.map((r) => r.email).join(';\n') +
+      ';';
 
     const emailFile = path.resolve(__dirname, '..', 'static', 'email', `${code}.txt`);
     fs.writeFileSync(emailFile, text);
