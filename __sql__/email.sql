12774bd0e89a155280cb15ce7298ba66831c81d3
Andrew Robertson <andrewcrobertson@gmail.com>
2021-05-05 12:32:19 +1000

    feat: wip

--- __sql__/email.sql
+++ __sql__/email.sql
@@ -4,49 +4,16 @@ FROM (
   FROM player p
   INNER JOIN playerContactGuardian pcg1 ON p.footyWebNumber = pcg1.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
-    AND p.footyWebNumber NOT IN (
-      '03530829', -- Isabella Crane
-      '01612058', -- Kaiyla Dybing
-      '01255889', -- Scarlett Barnett
-      '03043657', -- Tahlia Dannefaerd
-      '01364241', -- Viveca Kleinert
-      '02047322', -- Olivia Koch
-      '02873325', -- Lillie Marotti
-      '03221507', -- Jocelyn Thomas
-      '02953689'  -- Chelsea Hepburn
-    )
   UNION
   SELECT pcr.email1 as email
   FROM player p
   INNER JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
-    AND p.footyWebNumber NOT IN (
-      '03530829', -- Isabella Crane
-      '01612058', -- Kaiyla Dybing
-      '01255889', -- Scarlett Barnett
-      '03043657', -- Tahlia Dannefaerd
-      '01364241', -- Viveca Kleinert
-      '02047322', -- Olivia Koch
-      '02873325', -- Lillie Marotti
-      '03221507', -- Jocelyn Thomas
-      '02953689'  -- Chelsea Hepburn
-    )
   UNION
   SELECT pcr.email2 as email
   FROM player p
   INNER JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
-    AND p.footyWebNumber NOT IN (
-      '03530829', -- Isabella Crane
-      '01612058', -- Kaiyla Dybing
-      '01255889', -- Scarlett Barnett
-      '03043657', -- Tahlia Dannefaerd
-      '01364241', -- Viveca Kleinert
-      '02047322', -- Olivia Koch
-      '02873325', -- Lillie Marotti
-      '03221507', -- Jocelyn Thomas
-      '02953689'  -- Chelsea Hepburn
-    )
   UNION
   SELECT t.email FROM teamOfficial t
   UNION
