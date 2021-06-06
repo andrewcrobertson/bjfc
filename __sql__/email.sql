e634c60ed1e9ed9ebad5fe13cfefb9b2834e6115
Andrew Robertson <andrewcrobertson@gmail.com>
2021-06-06 20:14:40 +1000

    feat: wip

--- __sql__/email.sql
+++ __sql__/email.sql
@@ -4,16 +4,19 @@ FROM (
   FROM player p
   INNER JOIN playerContactGuardian pcg1 ON p.footyWebNumber = pcg1.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
+    OR p.footyWebNumber IN (SELECT footyWebNumber FROM transfer where transferYear = '2021' AND toClub = 'Bayswater')
   UNION
   SELECT pcr.email1 as email
   FROM player p
   INNER JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
+    OR p.footyWebNumber IN (SELECT footyWebNumber FROM transfer where transferYear = '2021' AND toClub = 'Bayswater')
   UNION
   SELECT pcr.email2 as email
   FROM player p
   INNER JOIN playerContactRegistered pcr ON p.footyWebNumber = pcr.footyWebNumber
   WHERE p.status in ('Registered', 'Insured')
+    OR p.footyWebNumber IN (SELECT footyWebNumber FROM transfer where transferYear = '2021' AND toClub = 'Bayswater')
   UNION
   SELECT t.email FROM teamOfficial t
   UNION
