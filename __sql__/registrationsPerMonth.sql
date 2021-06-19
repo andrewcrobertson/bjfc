913798a56a03c3974df7c9831ba30a3eaae3391a
Andrew Robertson <andrewcrobertson@gmail.com>
2021-06-19 10:51:36 +1000

    feat: wip

--- __sql__/registrationsPerMonth.sql
+++ __sql__/registrationsPerMonth.sql
@@ -7,10 +7,11 @@ FROM (
   WHERE p.club = 'Bayswater' AND t.product IN ('2021 Registration') and t.status = 'Paid'
     AND t.footyWebNumber NOT IN ( SELECT footyWebNumber FROM transfer where transferYear = '2021' AND toClub = 'Bayswater')
   UNION
-  SELECT footyWebNumber,
-    COALESCE(substr(finalisedDate, 1, 7), substr(applicationDate, 1, 7)) month
-  FROM transfer
-  where transferYear = '2021' AND toClub = 'Bayswater'
+  SELECT t.footyWebNumber,
+    COALESCE(substr(t.finalisedDate, 1, 7), substr(t.applicationDate, 1, 7)) month
+  FROM transfer t
+  INNER JOIN player p ON p.footyWebNumber = t.footyWebNumber
+  where t.transferYear = '2021' AND toClub = 'Bayswater'
 ) a
 GROUP BY a.month
 ORDER BY a.month;
